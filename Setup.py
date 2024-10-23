a2628f2f4d78103649d82fcfc59723eb9492645c
Aman Raju <68341884+rajuaman1@users.noreply.github.com>
2024-10-23 23:02:03 +0530

    Create Setup.py

---
+++ Setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='QNPy_Latte',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+    'astropy>=5.3.4',
+    'cycler>=0.11.0',
+    'dill>=0.3.4',
+    'matplotlib>=3.5.2',
+    'MiniSom>=2.3.1',
+    'numpy>=1.21.5',
+    'pandas>=1.4.4',
+    'plotly>=5.9.0',
+    'scikit_learn>=1.0.2',
+    'scipy>=1.9.1',
+    'seaborn>=0.12.2',
+    'torch>=1.13.0',
+    'tqdm>=4.64.1',
+    'tslearn>=0.6.2']
+    authors = 'Aman Raju, Marina Pavlovic, Andjelka Kovacevic, Dragana Ilic, Iva Cvorovic-Hajdinjak',
+    author_email='rajuaman@gmail.com',
+    description='Latent ATTEntive Neural Processes for Quasar Light Curves with parametric recovery',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/rajuaman1/QNPy_Latte',  # Project's URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
